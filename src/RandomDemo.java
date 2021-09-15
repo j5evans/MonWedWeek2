498aec5b8601bdd578500d2a2aaa2829252d7466
j5evans <j5evans@student.bridgew.edu>
2021-09-15 15:35:56 -0400

    RandomDemo revised (working)

--- src/RandomDemo.java
+++ src/RandomDemo.java
@@ -6,13 +6,20 @@ public class RandomDemo {
         var randomNumberPicker = new Random();
         var randomNumber = randomNumberPicker.nextInt(10)+1;
         var inputReader = new Scanner(System.in);
+
         System.out.print("Pick a random number from 1 to 10:");
-        var usersNumber = inputReader.nextInt();
-        if (randomNumber == usersNumber) {
-            System.out.println("You were right, the number was " + randomNumber);
-        }
-        else {
-            System.out.println("Unfortunately " + usersNumber + " was not the right number.");
+        while (true) {
+            var usersNumber = inputReader.nextInt();
+            if (randomNumber == usersNumber) {
+                System.out.println("You were right, the number was " + randomNumber + ".");
+                break;
+            }
+            else if (randomNumber < usersNumber) {
+                System.out.println("Your guess was too high. Try again:");
+            }
+            else if (randomNumber > usersNumber) {
+                System.out.println("Your guess was too low. Try again:");
+            }
         }
     }
 }
